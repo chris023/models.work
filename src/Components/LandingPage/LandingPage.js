a0baed576c6b5d8473091a16e7ae45e96ba00892
Jack Laird <jack.laird0@gmail.com>
2018-11-17 18:36:18 -0700

    Add basic react structure to component.js files

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './LandingPage.css';
+
+class LandingPage extends Component {
+
+  render() {
+    return (
+      <h2>Landing Page</h2>
+    )
+  }
+}
+
+export default LandingPage 

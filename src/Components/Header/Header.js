aa8bd371d5d670dedc0d115950a700021705541f
Jack Laird <jack.laird0@gmail.com>
2018-11-20 17:08:06 -0700

    Add a header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Header.scss';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className='Header'>
+        <NavLink exact to='/' className='nav logo' activeClassName='selected'> LOGO GOES HERE </NavLink>
+        <NavLink to='/eventForm'> REMEMBER TO DELETE THIS JACK</NavLink>
+        {
+          this.props.authenticated
+            ? <NavLink to='/logout' className='nav signout' activeClassName='selected'>Sign Out</NavLink>
+            : <NavLink to='/login' className='nav login' activeClassName='selected'>Login/Signup</NavLink>
+        }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated,
+});
+
+export default connect(mapStateToProps)(Header);

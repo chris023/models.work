b5aad764e38a77fc6208091b6add9d73a59fa7ba
Jack Laird <jack.laird0@gmail.com>
2018-11-27 13:27:53 -0700

    Add form to basicinfo2 component

--- src/Containers/Onboarding/ModelSide/BasicInfo2/BasicInfo2.jsx
+++ src/Containers/Onboarding/ModelSide/BasicInfo2/BasicInfo2.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './BasicInfo2.scss';
 
+import { NavLink } from 'react-router-dom';
+
 class BasicInfo2 extends Component {
 
   render() {
     return (
-      <h2>BasicInfo2</h2>
+      <div className="basic-info-2">
+        <input type="text" placeholder='DOB (MM/DD/YYYY)'/>
+        <p>Height</p>
+        <div className="height-input-container">
+          <input type="number" min='0'/>
+          <p>ft.</p>
+          <input type="number" min='0'/>
+          <p>in.</p>
+        </div>
+        <p>I model as a...</p>
+        <select name="" id="">
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <NavLink to='/onboard/model-onboard/biometrics'>Continue</NavLink>
+      </div>
     )
   }
 }

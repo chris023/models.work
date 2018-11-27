358ab247400fac89c408cbc97114d0f44dad91b3
Jack Laird <jack.laird0@gmail.com>
2018-11-27 13:27:41 -0700

    Add form to basicinfo component

--- src/Containers/Onboarding/ModelSide/BasicInfo/BasicInfo.jsx
+++ src/Containers/Onboarding/ModelSide/BasicInfo/BasicInfo.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './BasicInfo.scss';
 
+import { NavLink } from 'react-router-dom';
+
 class BasicInfo extends Component {
 
   render() {
     return (
-      <h2>BasicInfo</h2>
+      <div className="basic-info">
+        <input type="text" className="onboard-model-basicinfo-input" placeholder='First Name'/>
+        <input type="text" className="onboard-model-basicinfo-input" placeholder='Last Name'/>
+        <input type="text" className="onboard-model-basicinfo-input" placeholder='Phone'/>
+        <input type="text" className="onboard-model-basicinfo-input" placeholder='Zip Code'/>
+        <NavLink to='/onboard/model-onboard/add-photo'>Continue</NavLink>
+      </div>
     )
   }
 }

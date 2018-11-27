ade0072e5ed275a9b7e0a7f84179323c148ecdb2
Jack Laird <jack.laird0@gmail.com>
2018-11-27 12:40:11 -0700

    Create form for client side onboarding

--- src/Containers/Onboarding/ClientSide/ClientSide.jsx
+++ src/Containers/Onboarding/ClientSide/ClientSide.jsx
@@ -5,7 +5,20 @@ class ClientSide extends Component {
 
   render() {
     return (
-      <h2>ClientSide</h2>
+      <div className="ClientSide">
+        <input type="text" className="onboard-client-side-input" placeholder="Buisness Name"/>
+        <select name="" id="">
+          <option value="" disabled selected>Role</option>
+          <option value="Brand">Brand</option>
+          <option value="Distributor">Distributor</option>
+          <option value="Venue">Venue</option>
+        </select>
+        <p>Contact</p>
+        <input type="text" className="onboard-client-side-input" placeholder="Name"/>
+        <input type="text" className="onboard-client-side-input" placeholder="Phone"/>
+        <input type="text" className="onboard-client-side-input" placeholder="Email"/>
+        <button>Submit</button>
+      </div>
     )
   }
 }

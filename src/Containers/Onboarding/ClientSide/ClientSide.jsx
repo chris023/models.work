e1e513cfc104bbc5d16d443ec90d3d09d17fe0f9
Jack Laird <jack.laird0@gmail.com>
2018-11-26 20:26:57 -0700

    Add basic react structure to client side

---
+++ src/Containers/Onboarding/ClientSide/ClientSide.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './ClientSide.scss';
+
+class ClientSide extends Component {
+
+  render() {
+    return (
+      <h2>ClientSide</h2>
+    )
+  }
+}
+
+export default ClientSide;

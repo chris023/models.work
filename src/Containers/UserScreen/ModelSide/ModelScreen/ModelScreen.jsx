7a037f9de568fbc08ec839bb21accb75c09a9e98
Jack Laird <jack.laird0@gmail.com>
2018-11-28 15:17:24 -0700

    Add components and paths to userscreen modelscreen

--- src/Containers/UserScreen/ModelSide/ModelScreen/ModelScreen.jsx
+++ src/Containers/UserScreen/ModelSide/ModelScreen/ModelScreen.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './ModelScreen.scss';
+import { Route } from 'react-router-dom';
+
+//Components
+import Dashboard from '../Dashboard/Dashboard';
+import Events from '../Events/Events';
+import Footer from '../Footer/Footer';
+import Reports from '../Reports/Reports';
+import Wallet from '../Wallet/Wallet';
 
 class ModelScreen extends Component {
 
   render() {
     return (
-      <h2>ModelScreen</h2>
+      <div className="user-model-screen">
+        <Route
+            exact path='/user/  '
+            component={Dashboard} />
+        <Route
+            path='/user/events'
+            component={Events} />
+        <Route
+            path='/user/reports'
+            component={Reports} />
+        <Route
+            path='/user/wallet'
+            component={Wallet} />
+        <Route
+            path='/user/'
+            component={Footer} />
+      </div>
     )
   }
 }

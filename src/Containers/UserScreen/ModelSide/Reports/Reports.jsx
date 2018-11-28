f0eb377dfa0d87f5aabb8e218ab19a5a87caadde
Jack Laird <jack.laird0@gmail.com>
2018-11-28 14:24:44 -0700

    Add boilerplate to Reports component

---
+++ src/Containers/UserScreen/ModelSide/Reports/Reports.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Reports.scss';
+
+class Reports extends Component {
+
+  render() {
+    return (
+      <h2>Reports</h2>
+    )
+  }
+}
+
+export default Reports;

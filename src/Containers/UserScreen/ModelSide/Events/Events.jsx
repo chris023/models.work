9bc520b47be3d21568b0d8264955eb28dfff0c0e
Jack Laird <jack.laird0@gmail.com>
2018-11-28 14:24:25 -0700

    Add boilerplate to Event component

---
+++ src/Containers/UserScreen/ModelSide/Events/Events.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Events.scss';
+
+class Events extends Component {
+
+  render() {
+    return (
+      <h2>Events</h2>
+    )
+  }
+}
+
+export default Events;

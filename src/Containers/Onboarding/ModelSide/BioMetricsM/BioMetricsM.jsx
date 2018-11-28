bdc81dd22ad2799ab0f101205fc8303e43025c5e
Jack Laird <jack.laird0@gmail.com>
2018-11-28 11:10:00 -0700

    Add form content to BiometricsM component

--- src/Containers/Onboarding/ModelSide/BioMetricsM/BioMetricsM.jsx
+++ src/Containers/Onboarding/ModelSide/BioMetricsM/BioMetricsM.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './BioMetricsM.scss';
 
+import { NavLink } from 'react-router-dom';
+
 class BioMetricsM extends Component {
 
   render() {
     return (
-      <h2>BioMetricsM</h2>
+      <div className="bio-metrics-m">
+        <input type="text" placeholder='Chest '/>
+        <input type="text" placeholder='Collar'/>
+        <input type="text" placeholder='Inseam'/>
+        <input type="text" placeholder='Suit'/>
+        <input type="text" placeholder='Sleeve'/>
+        <NavLink to='/onboard/model-onboard/personal-bio'>Continue</NavLink>
+      </div>
     )
   }
 }

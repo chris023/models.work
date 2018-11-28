504b1b410966d1f3d39d7c9428f3d2dd42c79cdb
Jack Laird <jack.laird0@gmail.com>
2018-11-28 16:13:54 -0700

    Add content to Dashboard

--- src/Containers/UserScreen/ModelSide/Dashboard/Dashboard.jsx
+++ src/Containers/UserScreen/ModelSide/Dashboard/Dashboard.jsx
@@ -5,9 +5,26 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <h2>Dashboard</h2>
+      <div className="user-screen-model-dash">
+        <header className="user-screen-model-dash-header">
+          <div className="user-screen-model-dash-header-top">
+            <p className="user-screen-model-dash-greeting">Hi, Christian</p>
+          </div>
+          <div className="user-screen-model-dash-header-bottom">
+            <div className="user-screen-model-dash-upcoming-events-container">
+              <h1 className="user-screen-model-dash-upcoming-events-number">4</h1>
+              <p className="user-screen-model-dash-upcoming-events-label">Upcomping Events</p>
+            </div>
+            <div className="user-screen-model-dash-completed-events-container">
+              <p className="user-screen-model-dash-completed-events-label">Events Completed</p>
+            <h1 className="user-screen-model-dash-completed-events-number">12</h1>
+            </div>
+          </div>
+        </header>
+        <h1>CALENDAR FILLER</h1>
+      </div>
     )
   }
 }
 
-export default Dashboard;
+export default Dashboard; 

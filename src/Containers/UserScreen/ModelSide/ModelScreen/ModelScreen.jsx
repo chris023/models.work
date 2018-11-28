ca0a144e78be7dadf28c7641e361c8bbfd38d9b9
Jack Laird <jack.laird0@gmail.com>
2018-11-28 14:24:35 -0700

    Add boilerplate to ModelScreen component

---
+++ src/Containers/UserScreen/ModelSide/ModelScreen/ModelScreen.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './ModelScreen.scss';
+
+class ModelScreen extends Component {
+
+  render() {
+    return (
+      <h2>ModelScreen</h2>
+    )
+  }
+}
+
+export default ModelScreen;

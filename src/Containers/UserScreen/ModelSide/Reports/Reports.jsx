d3329242500d2a6c94f37314610eb624bc390fa8
chris023 <chris.meyer023@gmail.com>
2018-12-04 11:17:40 -0700

    Report basic styling

--- src/Containers/UserScreen/ModelSide/Reports/Reports.jsx
+++ src/Containers/UserScreen/ModelSide/Reports/Reports.jsx
@@ -19,8 +19,41 @@ class Reports extends Component {
             <button className="header_bottom__button">Submit</button>
           </div>
         </div>
-        <div className="Report">
-
+        <div className="report">
+          <div className="units_sold">
+            <p className="units_sold__title">Total Units Sold</p>
+            <div className="product">
+              <p className="product__name">Remy Martin 1738 Accord Royal Cognac</p>
+              <div className="product__input_container">
+                <input className="product__input" type="text" />
+                <p className="product__units">Units</p>
+              </div>
+            </div>
+            <div className="product">
+              <p className="product__name">Remy Martin VSOP</p>
+              <div className="product__input_container">
+                <input className="product__input" type="text" />
+                <p className="product__units">Units</p>
+              </div>
+            </div>
+          </div>
+          <div className="seperator"></div>
+          <div className="question-oneline">
+            <p className="question__text">On a scale of 1-5, how was the store traffic during your demo?</p>
+            <input className="question__input-small" type="text"/>
+          </div>
+          <div className="question-oneline">
+            <p className="question__text">How many customers approached?</p>
+            <input className="question__input-small" type="text" />
+          </div>
+          <div className="question-oneline">
+            <p className="question__text">How many customers sampled?</p>
+            <input className="question__input-small" type="text" />
+          </div>
+          <div className="question">
+            <p className="question__text">What was consumers feedback on the product (neat)?</p>
+            <textarea className="question__textarea"/>
+          </div>
         </div>
       </div>
     )

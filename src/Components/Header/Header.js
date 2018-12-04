af700fac3e13533ffc22425b21b7baabdb6b120a
chris023 <chris.meyer023@gmail.com>
2018-12-04 12:28:46 -0700

    Made header sticky

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -24,23 +24,29 @@ class Header extends Component {
   render() {
 
     const landingHeader = () => (
-      <div className="landing_header">
-        { this.props.authenticated
-            ? <NavLink to='/logout' className='flex-center landing_header__button landing_header__signout'>Sign Out</NavLink>
-            : <NavLink to='/login' className='flex-center landing_header__button landing_header__login'>Login</NavLink>
-        }
-      </div>
+      <React.Fragment>
+        <div className="landing_header">
+          { this.props.authenticated
+              ? <NavLink to='/logout' className='flex-center landing_header__button landing_header__signout'>Sign Out</NavLink>
+              : <NavLink to='/login' className='flex-center landing_header__button landing_header__login'>Login</NavLink>
+          }
+        </div>
+        <div className="mobile_header_filler--landing"></div>
+      </React.Fragment >
     )
 
     const appHeader = () => (
-      <div className="app_header">
-        <p className="app_header__nav" onClick={this.toggleSideNav}>Ham</p>
-        <p className="app_header__title">Models.work</p>
-        {
-          this.state.showSideNav &&
-          <Sidedrawer toggleSideNav={this.toggleSideNav} />
-        }
-      </div>
+      <React.Fragment>
+        <div className="app_header">
+          <p className="app_header__nav" onClick={this.toggleSideNav}>Ham</p>
+          <p className="app_header__title">Models.work</p>
+          {
+            this.state.showSideNav &&
+            <Sidedrawer toggleSideNav={this.toggleSideNav} />
+          }
+        </div>
+        <div className="mobile_header_filler--app"></div>
+      </React.Fragment>
     )
 
     return (

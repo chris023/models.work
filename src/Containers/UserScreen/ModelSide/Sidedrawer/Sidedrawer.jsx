233e75412928569797749545c0cb861e6b9d92f0
chris023 <chris.meyer023@gmail.com>
2018-12-04 11:32:08 -0700

    Fix class property to className

--- src/Containers/UserScreen/ModelSide/Sidedrawer/Sidedrawer.jsx
+++ src/Containers/UserScreen/ModelSide/Sidedrawer/Sidedrawer.jsx
@@ -11,7 +11,7 @@ export default class Sidedrawer extends Component {
         <div class="top_container">
           <div className="flex-center profile">
             <p className="profile__image">Img</p>
-            <div class="name">
+            <div className="name">
               <p className="name__firstname">Antoinette</p>
               <p className="name__lastname">Rodriguez</p>
             </div>

ab8f8da70e4cfd5c35c53eb12b715f17dbc62fae
chris023 <chris.meyer023@gmail.com>
2018-12-05 15:46:45 -0700

    Fix firebase dependancies

--- src/shared/firebase/config/firebase.js
+++ src/shared/firebase/config/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
   apiKey: "AIzaSyBSP4mX9vxaNvDeyRBk7SL_SGBUaoe_p1o",

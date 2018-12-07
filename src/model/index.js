510f7c51950ed793658eb0067da6bbe1c35cb208
chris023 <chris.meyer023@gmail.com>
2018-12-07 12:24:08 -0700

    Create model index file

---
+++ src/model/index.js
@@ -0,0 +1,15 @@
+import Dashboard from './Dashboard/Dashboard';
+import EditProfile from './EditProfile/EditProfile';
+import Events from './Events/Events';
+import Reports from './Reports/Reports';
+import Screen from './Screen/Screen';
+import Wallet from './Wallet/Wallet';
+
+export {
+  Dashboard,
+  EditProfile,
+  Events,
+  Reports,
+  Screen,
+  Wallet
+}

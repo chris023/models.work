16d0b777ece519d419cefeea0513ef70425fc843
chris023 <chris.meyer023@gmail.com>
2018-12-07 16:22:05 -0700

    convert 4tab to 2tab

--- src/model/Screen/Screen.jsx
+++ src/model/Screen/Screen.jsx
@@ -5,49 +5,49 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 
 //Components
 import {
-    Dashboard,
-    Events,
-    Reports,
-    Wallet,
-    WalletSettings,
-    EditProfile
+  Dashboard,
+  Events,
+  Reports,
+  Wallet,
+  WalletSettings,
+  EditProfile
 } from '../';
 
 import {
-    BottomNav,
-    TopAppBar,
+  BottomNav,
+  TopAppBar,
 } from '../../shared/ui';
 
 export default class Screen extends Component {
 
   render() {
-    return (
-        <MuiThemeProvider theme={theme}>
-            <Route
-                path='/user/'
-                component={TopAppBar} />
-            <Route
-                exact path='/user/dashboard'
-                component={Dashboard} />
-            <Route
-                exact path='/user/events'
-                component={Events} />
-            <Route
-                exact path='/user/reports'
-                component={Reports} />
-            <Route
-                exact path='/user/wallet'
-                component={Wallet} />
-            <Route
-                exact path='/user/wallet/settings'
-                component={WalletSettings} />
-            <Route 
-                exact path='/user/profile/edit'
-                    component={EditProfile} />
-            <Route
-                path='/user/'
-                component={BottomNav} />
-        </MuiThemeProvider>
-    )
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Route
+        path='/user/'
+        component={TopAppBar} />
+      <Route
+        exact path='/user/dashboard'
+        component={Dashboard} />
+      <Route
+        exact path='/user/events'
+        component={Events} />
+      <Route
+        exact path='/user/reports'
+        component={Reports} />
+      <Route
+        exact path='/user/wallet'
+        component={Wallet} />
+      <Route
+        exact path='/user/wallet/settings'
+        component={WalletSettings} />
+      <Route 
+        exact path='/user/profile/edit'
+          component={EditProfile} />
+      <Route
+        path='/user/'
+        component={BottomNav} />
+    </MuiThemeProvider>
+  )
   }
 }

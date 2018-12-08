a333263560046e70f790e1914b583fb4c7278d50
chris023 <chris.meyer023@gmail.com>
2018-12-08 12:53:37 -0700

    Add usenext for typography

--- src/model/Screen/Theme.js
+++ src/model/Screen/Theme.js
@@ -3,7 +3,10 @@ import { pink } from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: pink,
-  }
+  },
+  typography: {
+    useNextVariants: true,
+  },
 });
 
 theme.palette.primary.main = pink[400];

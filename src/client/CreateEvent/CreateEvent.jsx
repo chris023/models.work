d2966e78f1f985341f071a1579a29ba69484425f
Jack Laird <jack.laird0@gmail.com>
2018-12-12 13:24:47 -0700

    Add material ui to create event component

--- src/client/CreateEvent/CreateEvent.jsx
+++ src/client/CreateEvent/CreateEvent.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import { db } from '../../shared/firebase/config/firebase';
 
+// import Input from '@material-ui/core/Input';
+// import Select from '@material-ui/core/Select';
+
+import { Input, Select, TextField, FormControl } from '@material-ui/core';
+import { DatePicker, MuiPickersUtilsProvider } from 'material-ui-pickers';
+import DateFnsUtils from '@date-io/date-fns';
+// import { FormControl } from '@material-ui/core/FormControl';
+import { withStyles } from '@material-ui/core/styles';
 import './CreateEvent.scss';
 
-export default class CreateEvent extends Component {
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+class CreateEvent extends Component {
   constructor() {
     super();
     this.state = {
@@ -26,7 +51,7 @@ export default class CreateEvent extends Component {
     }
   }
 
-  handleCalendarSelection = eventDate => this.setState({ eventDate: eventDate.target.value });
+  handleCalendarSelection = date => this.setState({ eventDate: new Date(date) });
 
   handleInputChange = event => {
     const { name, value } = event.target;
@@ -46,6 +71,7 @@ export default class CreateEvent extends Component {
 
   render() {
     return (
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <div className="CreateEvent">
         <div className="header">
           <p className="header__title">Create Event</p>
@@ -53,8 +79,8 @@ export default class CreateEvent extends Component {
         </div>
         <div className="event_form" action="">
           <section className="event_info">
-            <input className="event_info__title" name='eventTitle' type="text" placeholder='Event Title' value={this.state.eventTitle} onChange={this.handleInputChange} />
-            <select className="event_info__event_type" defaultValue="" name='eventType' onChange={this.handleInputChange}>
+            <TextField variant="outlined" className="event_info__title" name='eventTitle' type="text" label='Event Title' value={this.state.eventTitle} onChange={this.handleInputChange} />
+            <Select className="event_info__event_type" defaultValue="Event Type" name='eventType' onChange={this.handleInputChange} value={this.state.eventType}>
               <option value='' disabled hidden>Event Type</option>
               <option value="On Premise">On Premise</option>
               <option value="Off premise">Off Premise</option>
@@ -62,47 +88,61 @@ export default class CreateEvent extends Component {
               <option value="Logistics" disabled>Logistics</option>
               <option value="Strike Crew" disabled>Strike Crew</option>
               <option value="Social Media" disabled>Social Media</option>
-            </select>
-            <input type="date" onChange={this.handleCalendarSelection} value={this.state.eventDate} />
-            <input name='eventTime' type="text" placeholder='Time' value={this.state.eventTime} onChange={this.handleInputChange} />
-            <input name='eventBrand' type="text" placeholder='Event Brand' value={this.state.eventBrand} onChange={this.handleInputChange} />
+            </Select>
+            {/* <TextField variant="outlined" type="date" onChange={this.handleCalendarSelection} value={this.state.eventDate} /> */}
+            <FormControl fullwidth>
+              <DatePicker
+                    clearable
+                    label="Choose Event Date"
+                    minDateMessage="Date must be greater than today"
+                    value={this.state.eventDate}
+                    onChange={this.handleCalendarSelection}
+                    animateYearScrolling={false}
+                    format="M/D/Y"
+                />
+            </FormControl>
+            <TextField name='eventTime' type="text" label='Time' value={this.state.eventTime} onChange={this.handleInputChange} />
+            <TextField variant="outlined" name='eventBrand' type="text" label='Event Brand' value={this.state.eventBrand} onChange={this.handleInputChange} />
           </section>
           <section className="venue_info">
             <p className="venue_info__title">Venue Info</p>
-            <input className="venue_info__name" name='venueName' type="text" placeholder='Venue Name' value={this.state.venueName} onChange={this.handleInputChange} />
-            <input className="venue_info__address" name='venueAddress' type="text" placeholder='Address' value={this.state.venueAddress} onChange={this.handleInputChange} />
+            <TextField variant="outlined" className="venue_info__name" name='venueName' type="text" label='Venue Name' value={this.state.venueName} onChange={this.handleInputChange} />
+            <TextField variant="outlined" className="venue_info__address" name='venueAddress' type="text" label='Address' value={this.state.venueAddress} onChange={this.handleInputChange} />
           </section>
           <section className="venue_contact">
             <p className="venue_contact__title">Venue Contact</p>
-            <input className="venue_contact__name" name='venueContactName' type="text" placeholder='Name' value={this.state.venueContactName} onChange={this.handleInputChange} />
-            <input className="venue_contact__email" name='venueContactEmail' type="text" placeholder='Email' value={this.state.venueContactEmail} onChange={this.handleInputChange} />
-            <input className="venue_contact__phone" name='venueContactPhone' type="text" placeholder='Phone Number' value={this.state.venueContactPhone} onChange={this.handleInputChange} />
+            <TextField variant="outlined" className="venue_contact__name" name='venueContactName' type="text" label='Name' value={this.state.venueContactName} onChange={this.handleInputChange} />
+            <TextField variant="outlined" className="venue_contact__email" name='venueContactEmail' type="text" label='Email' value={this.state.venueContactEmail} onChange={this.handleInputChange} />
+            <TextField variant="outlined" className="venue_contact__phone" name='venueContactPhone' type="text" label='Phone Number' value={this.state.venueContactPhone} onChange={this.handleInputChange} />
           </section>
           <section className="staffing">
             <div className="staff_1">
               <p className="staffing__title">Add Staffing</p>
-              <select className="staff__occupation" defaultValue="" name='employeeOccupation' onChange={this.handleInputChange}>
+              <Select className="staff__occupation" defaultValue="" name='employeeOccupation' onChange={this.handleInputChange}>
                 <option value='' disabled hidden>Staff Occupation</option>
                 <option value="Model">Model</option>
-              </select>
-              <select className="staff__role" defaultValue="" name='employeeRole' onChange={this.handleInputChange}>
+              </Select>
+              <Select className="staff__role" defaultValue="" name='employeeRole' onChange={this.handleInputChange}>
                 <option value='' disabled hidden>Role</option>
                 <option value="Brand Ambassador">Brand Ambassador</option>
-              </select>
-              <select className="staff__gender" defaultValue="" name='employeeGender' onChange={this.handleInputChange}>
+              </Select>
+              <Select className="staff__gender" defaultValue="" name='employeeGender' onChange={this.handleInputChange}>
                 <option value='' disabled hidden>Gender</option>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
-              </select>
-              <input className="staff__rate" name='employeePay' type="number" min='1' placeholder='Hourly Rate' onChange={this.handleInputChange} />
-              <input className="staff__number_to_hire" name='employeeNumber' type="number" min='1' placeholder='Number of Talent' onChange={this.handleInputChange} />
-              <input className="staff__attire" name='employeeAttire' type="text" placeholder='Attire' onChange={this.handleInputChange} />
-              <input className="staff__notes" name='employeeNotes' type="text" placeholder='Notes' onChange={this.handleInputChange} />
+              </Select>
+              <TextField variant="outlined" className="staff__rate" name='employeePay' type="number" min='1' label='Hourly Rate' onChange={this.handleInputChange} />
+              <TextField variant="outlined" className="staff__number_to_hire" name='employeeNumber' type="number" min='1' label='Number of Talent' onChange={this.handleInputChange} />
+              <TextField variant="outlined" className="staff__attire" name='employeeAttire' type="text" label='Attire' onChange={this.handleInputChange} />
+              <TextField variant="outlined" className="staff__notes" name='employeeNotes' type="text" label='Notes' onChange={this.handleInputChange} />
             </div>
             <p style={{ "textAlign": "center" }}>+</p> {/*placeholder*/}
           </section>
         </div>
       </div>
+      </MuiPickersUtilsProvider>
     )
   }
 }
+
+export default withStyles(styles)(CreateEvent)
